c3c3b2c88ffe54f5f73c1ec212f5be400ee7dfd9
George Sheng <gsheng@Georges-MacBook-Pro.local>
2020-06-07 04:58:19 -0400

    Added spinning to eggs

--- src/Egg.java
+++ src/Egg.java
@@ -1,5 +1,14 @@
+import java.awt.*;
+
 public class Egg extends Projectile{
+    private int counter = 0;
     public Egg(Map map, Vector location, Vector stepVector){
         super(map, location, stepVector, 132, 18, Util.EGG);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        counter += (int)(Math.random() * 2) + 1;
+        g.drawImage(Util.rotateDegrees(Util.EGG, counter), (int)getLocation().x, (int)getLocation().y, null);
     }
 }
